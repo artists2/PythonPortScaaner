dcaf4021970412391aee9b13933a95656770664e
r00t0k <artists2@naver.com>
2020-10-21 12:49:00 +0900

    python 2v -> python 3v

--- PortScaner.py
+++ PortScaner.py
@@ -10,29 +10,29 @@ def ConnScan(Target_Host, Target_Port):
         connskt.connect((Target_Host, Target_Port)) 
         connskt.send('Vionlent python\r\n')
         result = connskt.recv(1024) # 값 결과저장
-        print "[+] %d / TCP OPEN" % Target_Port
-        print "\n" + str(result) 
+        print ("[+] %d / TCP OPEN") % Target_Port
+        print ("\n") + str(result) 
         connskt.close()
     except: # 예외처리 
-        print "[-] %d TCP CLosed\n" % Target_Port
+        print ("[-] %d TCP CLosed\n") % Target_Port
  
 
 def PortScan(Target_Host, Target_Port):
     try: # 예외처리 
         Target_IP = gethostbyname(Target_Host) 
     except:
-        print "[-] Cannot Resolve '%s' : Unknown Host\n" % Target_Host
+        print ("[-] Cannot Resolve '%s' : Unknown Host\n") % Target_Host
         return
  
     try:
         Target_Name = gethostbyname(Target_IP) 
-        print "\n[+] Scan Result For : " + Target_Name[0] # 출력 
+        print ("\n[+] Scan Result For : ") + Target_Name[0] # 출력 
     except:
-        print "\n[+] Scan Result For : " + Target_IP
+        print ("\n[+] Scan Result For : " )+ Target_IP
     setdefaulttimeout(1)
  
     for port in Target_Port: # 여러개의 포트를 입력받을수 있도록 함 
-        print "Scanning Port \n" + port
+        print ("Scanning Port \n") + port
         t = Thread (target=ConnScan, args=(Target_Host, int(port))) # t 메소드를 스레드로 정의한다 스레드의 내용물은 target=ConnScan, args=(Target_Host, int(port)?
         t.start() # start?
  
@@ -48,7 +48,7 @@ def main(): # Main함수 optparser를 통한 파싱작업을 하여 사용자에
     Target_Port = str(options.Target_Port).split(',')
  
     if (Target_Host == None) | (Target_Port == None):
-        print parser.usage
+        print (parser.usage)
         exit (0)
     PortScan(Target_Host, Target_Port)
  
